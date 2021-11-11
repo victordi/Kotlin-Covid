e11664386d104527db9dc8475bd68da43d09acb0
Victor Diaconu <victor.diaconu@ing.com>
2021-11-11 15:10:17 +0200

    Using runCatching instead of a try/catch block

--- src/main/kotlin/com/idp/parser/CsvParser.kt
+++ src/main/kotlin/com/idp/parser/CsvParser.kt
@@ -6,68 +6,50 @@ import com.idp.model.Data
 import com.idp.model.National
 import com.idp.model.State
 import java.io.File
-import java.lang.NumberFormatException
 import java.time.LocalDate
-import java.time.format.DateTimeParseException
 
 inline fun <reified T : Data, R> CsvReader.read(file: File, crossinline seqF: Sequence<T>.() -> Sequence<R>): List<R> =
     open(file) {
-        readAllWithHeaderAsSequence()
-            .mapNotNull { it.toData<T>() }
+        readAllWithHeaderAsSequence().runCatching {
+            mapNotNull { it.toData<T>() }
             .seqF()
             .toList()
+        }.getOrDefault(emptyList())
     }
 
-inline fun <reified T> Map<String, String>.toData(): T? = when (T::class) {
-    State::class    -> toState()
-    County::class   -> toCounty()
+inline fun <reified T : Data> Map<String, String>.toData(): T? = when (T::class) {
+    State::class -> toState()
+    County::class -> toCounty()
     National::class -> toNational()
-    else            -> TODO()
+    else -> TODO()
 } as T?
 
-fun Map<String, String>.toNational(): National? = try {
+fun Map<String, String>.toNational(): National? = runCatching {
     National(
-        date   = LocalDate.parse(getValue("date")),
-        cases  = getValue("cases").toInt(),
+        date = LocalDate.parse(getValue("date")),
+        cases = getValue("cases").toInt(),
         deaths = getValue("deaths").toInt()
     )
-} catch (e: NumberFormatException) {
-    null
-} catch (e: DateTimeParseException) {
-    null
-} catch (e: NoSuchElementException) {
-    null
-}
+}.getOrNull()
 
-fun Map<String, String>.toState(): State? = try {
+
+fun Map<String, String>.toState(): State? = runCatching {
     State(
-        date   = LocalDate.parse(getValue("date")),
-        cases  = getValue("cases").toInt(),
+        date = LocalDate.parse(getValue("date")),
+        cases = getValue("cases").toInt(),
         deaths = getValue("deaths").toInt(),
-        state  = getValue("state"),
-        fips   = getValue("fips").toInt()
+        state = getValue("state"),
+        fips = getValue("fips").toInt()
     )
-} catch (e: NumberFormatException) {
-    null
-} catch (e: DateTimeParseException) {
-    null
-} catch (e: NoSuchElementException) {
-    null
-}
+}.getOrNull()
 
-fun Map<String, String>.toCounty(): County? = try {
+fun Map<String, String>.toCounty(): County? = runCatching {
     County(
-        date   = LocalDate.parse(getValue("date")),
-        cases  = getValue("cases").toInt(),
+        date = LocalDate.parse(getValue("date")),
+        cases = getValue("cases").toInt(),
         deaths = getValue("deaths").toInt(),
-        state  = getValue("state"),
-        fips   = getValue("fips").toInt(),
+        state = getValue("state"),
+        fips = getValue("fips").toInt(),
         county = getValue("county")
     )
-} catch (e: NumberFormatException) {
-    null
-} catch (e: DateTimeParseException) {
-    null
-} catch (e: NoSuchElementException) {
-    null
-}
+}.getOrNull()
